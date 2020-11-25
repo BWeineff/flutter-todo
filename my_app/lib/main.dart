9dde20fb9fdfdbd040f0b4981747cc1c761427ae
Benjamin Weinberger <benjaminweinberger@gmail.com>
2020-11-24 21:54:14 -0700

    built out auth.dart service for wiring up to the main.dart file of _RootState

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -2,23 +2,54 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+// Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+import 'package:my_app/services/auth.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(App());
+}
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Welcome to the Thunderdome',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to the Thunderdome'),
-          ),
-          body: const Center(
-            child: Text('Hello World'),
-          ),
-        ));
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return const Scaffold(body: Center(child: Text("Error"),),);
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Root();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return const Scaffold(body: Center(child: Text("Loading..."),),);
+      },
+    );
   }
 }
-// https://youtu.be/aiTTClKJbnw?t=481 left off here
+
+class Root extends StatefulWidget {
+  @override
+  _RootState createState() => _RootState();
+}
+
+class _RootState extends State<Root> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(stream:Auth(auth: _auth).user, 
+    builder: (BuildContext context, AsyncSnapshot<User> snapshot) {  }, //Auth stream      
+    );
+  }
+}
+
+// TODO __ left off here with inactive vs active builder stream https://youtu.be/aiTTClKJbnw?t=913 
